1a71a4902e85a734d143ec524f78f4ed587228e0
JoaquinCorcheroFMP <jcorchero@findmypast.com>
2016-05-03 09:20:53 +0100

    Removed exercism

--- exercism.io/2.aliases.ex
+++
@@ -1,8 +0,0 @@
-defmodule AliasModule do
-    alias IO, as: MyIO
-    
-    def print(to_print) do
-        MyIO.puts "Printing the string with alias "
-        MyIO.puts to_print
-    end
-end
--- exercism.io/2.imports.ex
+++
@@ -1,8 +0,0 @@
-defmodule ImportModule do
-    import IO
-    
-    def print(to_print) do
-        puts "Printing the string with import "
-        puts to_print
-    end
-end
